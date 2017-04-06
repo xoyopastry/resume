62518c5c3b1882f2f7c820ba40d4b3b34e09dfc0
libin <1097843969@qq.com>
2017-04-06 12:06:25 +0800

    结构建造完成

---
+++ js/resume.js
@@ -0,0 +1,17 @@
+/***
+resume.js
+**/
+
+/*swiper cont*/
+
+var swiperH = new Swiper('.swiper-container-h', {
+        pagination: '.swiper-pagination-h',
+        paginationClickable: true,
+        spaceBetween: 50
+    });
+var swiperV = new Swiper('.swiper-container-v', {
+        pagination: '.swiper-pagination-v',
+        paginationClickable: true,
+        direction: 'vertical',
+        spaceBetween: 50
+    });
